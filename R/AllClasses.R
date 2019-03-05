5254c0418a061ba2fd5c3ea283c170312b1bcb69
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2019-03-05 15:46:39 -0500

    Updates made to GeneSet 	- Added roxygen documentation to GeneSet class 	- Working on vignette to reflect GeneSet()

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -4,18 +4,17 @@ setOldClass("tbl_set")
 
 setOldClass("tbl_geneset")
 
+#' @rdname geneset
+#'
+#' @slot gene The gene tibble from `tbl_geneset`
+#' @slot set The set tibble from `tbl_geneset`
+#' @slot geneset The geneset tibble created from user input
+#' @slot active The tibble that is active
+#'
+#' @exportClass GeneSet
+
 .GeneSet <- setClass(
     "GeneSet",
-    slots = c(
-        gene = "tbl_gene",
-        set = "tbl_set",
-        geneset = "tbl_geneset",
-        active = "character"
-    )
-)
-
-.GeneSet2 <- setClass(
-    "GeneSet2",
     slots = c(
         gene = "tbl_gene",
         set = "tbl_set",
