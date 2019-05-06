93562d8ebb3be70fcf30035f63ee7e62b3f4ecc4
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2019-05-06 15:20:13 -0400

    Changed return of `union()` and `intersect()` 	- Now return BiocSet object not single tibble 	- removed wrappers for `union()` and `intersect()`

--- R/set_ops.R
+++ R/set_ops.R
@@ -15,55 +15,10 @@
 #' union(es1, es2)
 union.BiocSet <- function(x, y, ...)
 {
-    sub_x <- .active_value(x)
-    sub_y <- .active_value(y)
-    tbl <- union(sub_x, sub_y, ...)
-    #initialize(x, .active(x) = tbl)
-}
-
-#' @rdname set_op
-#'
-#' @export
-#'
-#' @examples
-#' es1 <- BiocSet(set1 = letters[c(1:3)], set2 = LETTERS[c(1:3)])
-#' es2 <- BiocSet(set1 = letters[c(2:4)], set2 = LETTERS[c(2:4)])
-#' union_element(es1, es2)
-union_element <- function(x, y, ...)
-{
-    sub_x <- es_activate(x, "element")
-    sub_y <- es_activate(y, "element")
-    union(sub_x, sub_y, ...)
-}
-
-#' @rdname set_op
-#'
-#' @export
-#' 
-#' @examples
-#' es1 <- BiocSet(set1 = letters[c(1:3)], set2 = LETTERS[c(1:3)])
-#' es2 <- BiocSet(set1 = letters[c(2:4)], set2 = LETTERS[c(2:4)])
-#' union_set(es1, es2)
-union_set <- function(x, y, ...)
-{
-    sub_x <- es_activate(x, "set")
-    sub_y <- es_activate(y, "set")
-    union(sub_x, sub_y)
-}
-
-#' @rdname set_op
-#'
-#' @export
-#'
-#' @examples
-#' es1 <- BiocSet(set1 = letters[c(1:3)], set2 = LETTERS[c(1:3)])
-#' es2 <- BiocSet(set1 = letters[c(2:4)], set2 = LETTERS[c(2:4)])
-#' union_elementset(es1, es2)
-union_elementset <- function(x, y, ...)
-{
-    sub_x <- es_activate(x, "elementset")
-    sub_y <- es_activate(y, "elementset")
-    union(sub_x, sub_y)
+    element <- union(es_element(x), es_element(y), ...)
+    set <- union(es_set(x), es_set(y), ...)
+    elementset <- union(es_elementset(x), es_elementset(y), ...)
+    initialize(x, element = element, set = set, elementset = elementset)    
 }
 
 #' @rdname set_op
@@ -78,53 +33,9 @@ union_elementset <- function(x, y, ...)
 #' intersect(es1, es2)
 intersect.BiocSet <- function(x, y, ...)
 {
-    sub_x <- .active_value(x)
-    sub_y <- .active_value(y)
-    tbl <- intersect(sub_x, sub_y, ...)
-    tbl
+    element <- intersect(es_element(x), es_element(y), ...)
+    set <- intersect(es_set(x), es_set(y), ...)
+    elementset <- intersect(es_elementset(x), es_elementset(y), ...)
+    initialize(x, element = element, set = set, elementset = elementset)
 }
 
-#' @rdname set_op
-#'
-#' @export
-#' 
-#' @examples
-#' es1 <- BiocSet(set1 = letters[c(1:4)], set2 = LETTERS[c(1:4)])
-#' es2 <- BiocSet(set1 = letters[c(3:8)], set2 = LETTERS[c(3:8)])
-#' intersect_element(es1, es2)
-intersect_element <- function(x, y, ...)
-{
-    sub_x <- es_activate(x, "element")
-    sub_y <- es_activate(y, "element")
-    intersect(sub_x, sub_y)
-}
-
-#' @rdname set_op
-#'
-#' @export
-#' 
-#' @examples
-#' es1 <- BiocSet(set1 = letters[c(1:4)], set2 = LETTERS[c(1:4)])
-#' es2 <- BiocSet(set1 = letters[c(3:8)], set2 = LETTERS[c(3:8)])
-#' intersect_set(es1, es2)
-intersect_set <- function(x, y, ...)
-{
-    sub_x <- es_activate(x, "set")
-    sub_y <- es_activate(y, "set")
-    intersect(sub_x, sub_y)
-}
-
-#' @rdname set_op
-#'
-#' @export
-#'
-#' @examples
-#' es1 <- BiocSet(set1 = letters[c(1:4)], set2 = LETTERS[c(1:4)])
-#' es2 <- BiocSet(set1 = letters[c(3:8)], set2 = LETTERS[c(3:8)])
-#' intersect_elementset(es1, es2)
-intersect_elementset <- function(x, y, ...)
-{
-    sub_x <- es_activate(x, "elementset")
-    sub_y <- es_activate(y, "elementset")
-    intersect(sub_x, sub_y)
-}
