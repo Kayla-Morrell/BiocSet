9516af99901e4eea334dce6146ce883486d16aa3
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2019-05-07 15:19:35 -0400

    Additional functions implemented 	- Improved unit tests for union and intersect 	- Added in go_sets, es_map, and kegg_sets

--- tests/testthat/test_set_ops.R
+++ tests/testthat/test_set_ops.R
@@ -9,25 +9,11 @@ test_that("'union.BiocSet()' works",
     expect_s4_class(es2, "BiocSet")
 
     tbl <- union(es1, es2)
-    expect_s3_class(tbl, "tbl_elementset")
-    expect_identical(class(tbl), c("tbl_elementset", "tbl_elementset_base",
-                                    "tbl_df", "tbl", "data.frame"))
-    expect_identical(dim(tbl), c(12L, 2L))
-    expect_true(is_tbl_elementset(tbl))
-
-    tbl2 <- union(es_activate(es1, "element"), es_activate(es2, "element"))
-    expect_s3_class(tbl2, "tbl_element")
-    expect_identical(class(tbl2), c("tbl_element", "tbl_elementset_base", 
-                                     "tbl_df", "tbl", "data.frame"))
-    expect_identical(dim(tbl2), c(12L, 1L))
-    expect_false(is_tbl_elementset(tbl2))
-
-    tbl3 <- union(es_activate(es1, "set"), es_activate(es2, "set"))
-    expect_s3_class(tbl3, "tbl_set")
-    expect_identical(class(tbl3), c("tbl_set", "tbl_elementset_base",
-                                     "tbl_df", "tbl", "data.frame"))
-    expect_identical(dim(tbl3), c(2L, 1L))
-    expect_false(is_tbl_elementset(tbl3))
+    expect_s4_class(tbl, "BiocSet")
+    expect_identical(dim(es_element(tbl)), c(12L, 1L))
+    expect_identical(dim(es_set(tbl)), c(2L, 1L))
+    expect_identical(dim(es_elementset(tbl)), c(12L, 2L))
+    expect_true(is_tbl_elementset(es_elementset(tbl)))
 })
 
 test_that("'intersect.BiocSet()' works", {
@@ -38,23 +24,9 @@ test_that("'intersect.BiocSet()' works", {
     expect_s4_class(es2, "BiocSet")
 
     tbl <- intersect(es1, es2)
-    expect_s3_class(tbl, "tbl_elementset")
-    expect_identical(class(tbl), c("tbl_elementset", "tbl_elementset_base",
-                                    "tbl_df", "tbl", "data.frame"))
-    expect_identical(dim(tbl), c(4L, 2L))
-    expect_true(is_tbl_elementset(tbl))
-
-    tbl2 <- intersect(es_activate(es1, "element"), es_activate(es2, "element"))
-    expect_s3_class(tbl2, "tbl_element")
-    expect_identical(class(tbl2), c("tbl_element", "tbl_elementset_base",
-                                     "tbl_df", "tbl", "data.frame"))
-    expect_identical(dim(tbl2), c(4L, 1L))
-    expect_false(is_tbl_elementset(tbl2))
-
-    tbl3 <- intersect(es_activate(es1, "set"), es_activate(es2, "set"))
-    expect_s3_class(tbl3, "tbl_set")
-    expect_identical(class(tbl3), c("tbl_set", "tbl_elementset_base",
-                                     "tbl_df", "tbl", "data.frame"))
-    expect_identical(dim(tbl3), c(2L, 1L))
-    expect_false(is_tbl_elementset(tbl3))
+    expect_s4_class(tbl, "BiocSet")
+    expect_identical(dim(es_element(tbl)), c(4L, 1L))
+    expect_identical(dim(es_set(tbl)), c(2L, 1L))
+    expect_identical(dim(es_elementset(tbl)), c(4L, 2L))
+    expect_true(is_tbl_elementset(es_elementset(tbl)))
 })
