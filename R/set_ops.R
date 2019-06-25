74057306e67f313a194848060ea20b05bdc6f08c
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2019-06-25 18:03:03 -0400

    Changes made to union and intersect 	- seperate functions depending on 	one argument or two

--- R/set_ops.R
+++ R/set_ops.R
@@ -1,26 +1,12 @@
-union_1arg <- function(x, ...)
-{
-    #unique_list <- unique(es_elementset(x)$element)
-    #x %>% filter_element(element %in% unique_list) %>% es_element()
-    x %>% es_element()
-}
-
-union_2arg <- function(x, y, ...)
-{
-    element <- union(es_element(x), es_element(y), ...)
-    set <- union(es_set(x), es_set(y), ...)
-    elementset <- union(es_elementset(x), es_elementset(y), ...)
-    initialize(x, element = element, set = set, elementset = elementset)
-}
-#how should union be imported, or does it even need to be?
-
 #' Set operations for BiocSets
 #'
 #' @rdname set_op
 #'
 #' @param x The first BiocSet to perform operations on
-#' @param y Optional. The second BiocSet object used
+#' @param y The second BiocSet object used
 #' @param ... other arguments passed on to methods
+#'
+#' @importFrom dplyr union
 #' 
 #' @return For union, a tibble with the results of the union of x or x and y.
 #' 
@@ -30,49 +16,67 @@ union_2arg <- function(x, y, ...)
 #' es1 <- BiocSet(set1 = letters[c(1:3)], set2 = LETTERS[c(1:3)])
 #' es2 <- BiocSet(set1 = letters[c(2:4)], set2 = LETTERS[c(2:4)])
 #' union(es1, es2)
-#' es3 <- BiocSet(set1 = letters[c(1:10)], set2 = letters[c(4:20)])
-#' union(es3)
-union.BiocSet <- function(x, y=NULL, ...)
+union.BiocSet <- function(x, y, ...)
 {
-    if (is.null(y))
-        union_1arg(x, ...)
-    else
-        union_2arg(x, y, ...)
-}
-
-intersect_1arg <- function(x, ...)
-{
-    elements <- es_elementset(x)$element
-    dup_list <- elements[duplicated(elements)]
-    x %>% filter_element(element %in% dup_list) %>% es_element()
-}
-
-intersect_2arg <- function(x, y, ...)
-{
-    element <- intersect(es_element(x), es_element(y), ...)
-    set <- intersect(es_set(x), es_set(y), ...)
-    elementset <- intersect(es_elementset(x), es_elementset(y), ...)
+    element <- union(es_element(x), es_element(y), ...)
+    set <- union(es_set(x), es_set(y), ...)
+    elementset <- union(es_elementset(x), es_elementset(y), ...)
     initialize(x, element = element, set = set, elementset = elementset)
 }
-# how should intersect and duplicate be imported or do they even need to be?
 
 #' @rdname set_op
 #'
+#' @return A BiocSet object with a single set and unioned elements
+#'
+#' @export
+#'
+#' @examples
+#' es1 <- BiocSet(set1 = letters[c(1:10)], set2 = letters[c(4:20)])
+#' union_single(es1) 
+union_single.BiocSet <- function(x, ...)
+{
+    unique_elements <- x %>% es_element() %>% pull(element)
+
+    BiocSet(union = unique_elements)
+}
+
+#' @rdname set_op
+#'
+#' @importFrom dplyr intersect
 #' 
 #' @return For intersect, a tibble with the intersect of x and y 
+#'
 #' @export
 #'
 #' @examples
 #' es1 <- BiocSet(set1 = letters[c(1:4)], set2 = LETTERS[c(1:4)])
 #' es2 <- BiocSet(set1 = letters[c(3:8)], set2 = LETTERS[c(3:8)])
 #' intersect(es1, es2)
-#' es3 <- BiocSet(set1 = letters[c(1:10)], set2 = letters[c(4:20)])
-#' intersect(es3)
-intersect.BiocSet <- function(x, y=NULL, ...)
+intersect.BiocSet <- function(x, y, ...)
 {
-    if (is.null(y))
-        intersect_1arg(x, ...)
-    else
-        intersect_2arg(x, y, ...)
+    element <- intersect(es_element(x), es_element(y), ...)
+    set <- intersect(es_set(x), es_set(y), ...)
+    elementset <- intersect(es_elementset(x), es_elementset(y), ...)
+    initialize(x, element = element, set = set, elementset = elementset)
 }
 
+#' @rdname set_op
+#'
+#' @return A BiocSet object with a single set and interesected elements
+#'
+#' @return
+#'
+#' @examples
+#' es1 <- BiocSet(set1 = letters[c(1:10)], set2 = letters[c(4:20)])
+#' intersect(es1) 
+intersect_single.BiocSet <- function(x, ...)
+{
+    elements <- es_elementset(x)$element
+    dup_list <- elements[duplicated(elements)]
+    dup_elements <- x %>% 
+        filter_element(element %in% dup_list) %>% 
+        es_element() %>%
+        pull(element)
+
+    BiocSet(intersect = dup_elements) 
+}
