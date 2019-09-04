7c7f156a1a971a7cac53f9432f3e6af932d4084e
Martin Morgan <martin.morgan@roswellpark.org>
2019-09-04 11:47:56 -0400

    use 'dme' rather than 'hsa' for faster kegg_set test

--- tests/testthat/test_mapping_set.R
+++ tests/testthat/test_mapping_set.R
@@ -51,14 +51,14 @@ test_that("'go_sets()' works",
 
 test_that("'kegg_sets()' works",
 {
-    es <- kegg_sets("hsa")
+    es <- kegg_sets("dme")
 
     expect_s4_class(es, "BiocSet")
     expect_identical(dim(es_element(es))[2], c(1L))
-    expect_gt(dim(es_element(es))[1], c(7896L))
-    expect_identical(dim(es_set(es)), c(326L, 1L))
+    expect_gt(dim(es_element(es))[1], c(3201L))
+    expect_identical(dim(es_set(es)), c(132L, 1L))
     expect_identical(dim(es_elementset(es))[2], c(2L))
-    expect_gt(dim(es_elementset(es))[1], c(29296L))
+    expect_gt(dim(es_elementset(es))[1], c(5345L))
     expect_true(.is_tbl_elementset(es_elementset(es)))
 
     expect_error(kegg_sets(hsa))
